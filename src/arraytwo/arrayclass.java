63063f617132a5864d6ac9ca35e84ca059ea5734
MashaMartin <59993828+MashaMartin@users.noreply.github.com>
2022-08-21 20:14:13 +0200

    Add files via upload

---
+++ src/arraytwo/arrayclass.java
@@ -0,0 +1,15 @@
+package arraytwo;
+
+public class arrayclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums = {5,8,12,84,55};
+		
+		int x = nums[2];
+		System.out.println(x);
+
+	}
+
+}
